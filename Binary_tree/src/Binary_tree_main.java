be4c4615e5b671961a6a019501da332c17de8645
puneet01994 <puneetchhapola@gmail.com>
2018-04-15 14:54:35 +0530

    Binary_tree BFS insertion Along-with traversals

--- Binary_tree/src/Binary_tree_main.java
+++ Binary_tree/src/Binary_tree_main.java
@@ -3,19 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package binary_tree;
 
 /**
  *
  * @author puneet
  */
-public class Binary_tree {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Binary_tree_main {
     
 }
